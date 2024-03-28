6f07f201e5912cc53b9fd65cc6b88aeef4200c48
NijatS <nijatjs@code.edu.az>
2024-03-28 13:29:11 +0400

    add:MultipleLayout

--- src/app/admin/components/dasboard/dasboard.module.ts
+++ src/app/admin/components/dasboard/dasboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DasboardComponent } from './dasboard.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,10 @@ import { DasboardComponent } from './dasboard.component';
     DasboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path:"",component:DasboardComponent}
+    ])
   ]
 })
 export class DasboardModule { }
